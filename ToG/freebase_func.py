d788a9c4078d27090d32f462b9c513e7cbd6dd92
Junhong Wan <31362319+wanjunhong0@users.noreply.github.com>
2024-04-08 19:54:55 +0800

    remote sparql server

--- ToG/freebase_func.py
+++ ToG/freebase_func.py
@@ -2,7 +2,7 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 from utils import *
 import random
 
-SPARQLPATH = "http://localhost:8890/sparql"  # depend on your own internal address and port, shown in Freebase folder's readme.md
+SPARQLPATH = "http://10.3.216.75:25815/sparql"  # depend on your own internal address and port, shown in Freebase folder's readme.md
 
 # pre-defined sparqls
 sparql_head_relations = """\nPREFIX ns: <http://rdf.freebase.com/ns/>\nSELECT ?relation\nWHERE {\n  ns:%s ?relation ?x .\n}"""
